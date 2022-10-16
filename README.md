5f2b83854046d747b9455f014d2e5cd1387338d8
Anton Brylyakov <yoll@rambler.ru>
2022-10-15 20:27:44 -0700

    Начальный коммит

---
+++ README.md
@@ -0,0 +1,2 @@
+# Задача 1. Простейший проект
+[Условие задачи](https://github.com/netology-code/cppm-homeworks/tree/main/10/01)
---
+++ main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+#ifdef _WIN32
+	#include "windows.h"
+#endif
+
+int main()
+{
+#ifdef _WIN32
+	SetConsoleCP(1251);
+#endif
+
+	setlocale(LC_ALL, "Russian");
+	std::string name;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!" << std::endl;
+	return 0;
+}
